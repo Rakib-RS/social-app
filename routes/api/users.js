43778d41a89aefb283033aceaff3e0a5af5c0f48
rakib-rs <rakib.rscse@gmail.com>
2020-03-24 16:26:51 +0600

    validation handler 2

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,22 +5,22 @@ const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const key = require("../../configure/key");
-const passport = require('passport');
-const validateRegisterInput = require('../../validation/register')
+const passport = require("passport");
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 router.get("/test", (req, res) => res.json({ msg: "User works" }));
 
 router.post("/register", (req, res) => {
-  const {errors,isValid} = validateRegisterInput(req.body);
-  if(!isValid){
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
     return res.status(400).json(errors);
   }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-       errors.email= 'email already exist';
+      errors.email = "email already exist";
       return res.json(errors);
-    }
-    else {
+    } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
         r: "pg", //ratting
@@ -50,8 +50,13 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) return res.status(400).json(errors);
   User.findOne({ email }).then(user => {
-    if (!user) return res.status(404).json({ email: "user not found" });
+    if (!user) {
+      errors.email = "user not found";
+      return res.status(404).json(errors);
+    }
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
         const payload = {
@@ -71,7 +76,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        res.status(404).json({ password: "password is incorrect" });
+        errors.password = "password is incorrect";
+        res.status(404).json(errors);
       }
     });
   });
@@ -80,12 +86,15 @@ router.post("/login", (req, res) => {
 //@route get api/users/current
 //@desc return current user
 //@@ access private
-router.get('/current',passport.authenticate('jwt',{session:false}),(req,res)=>{
-  res.json({
-    id: req.user.id,
-    name: req.user.name,
-    email: req.user.email
-
-  })
-})
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 module.exports = router;
--- validation/register.js
+++ validation/register.js
@@ -4,8 +4,31 @@ const  isEmpty = require('./is-empty')
 
 module.exports =  function validateRegisterInput(data){
     let errors ={};
+    data.name = !isEmpty(data.name) ? data.name : '';
+    data.email = !isEmpty(data.email) ? data.email : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+    data.password2 = !isEmpty(data.password2) ? data.password2: '';
+
     if(!validator.isLength(data.name,{min:2,max:30})){
         errors.name = 'name must be between 2 and 30 characters'
+    }
+    if(validator.isEmpty(data.name)){
+        errors.name = 'name field is required'
+    }
+    if(validator.isEmpty(data.email)){
+        errors.email = 'email field is required'
+    }
+    if(!validator.isEmail(data.email)){
+        errors.email = 'email is invalid'
+    }
+    if(validator.isEmpty(data.password)){
+        errors.password = 'password field is required'
+    }
+    if(validator.isEmpty(data.password2)){
+        errors.password2 = 'password2 field is required'
+    }
+    if(!validator.equals(data.password,data.password2)){
+        errors.match = 'password did not match';
     }
 
     return {
