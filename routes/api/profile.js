7edc81569f017ae0679dc6bfc8c2efb131d6de52
rakib-rs <rakib.rscse@gmail.com>
2020-03-25 12:37:26 +0600

    current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+const Profile = require('../../models/profile');
+const User = require('../../models/user');
 
-router.get('/test',(req,res)=> res.json({msg:'profile works'}))
+router.get('/test',(req,res)=> res.json({msg:'profile works'}));
+
+//@route get api/profile/
+//@desc return profile
+//@@ access private
+
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    const errors ={};
+    Profile.findOne({user: req.user.id})
+            .then(profile =>{
+                if(!profile){
+                    errors.noprofile = 'no profile found';
+                    return res.status(404).json(errors);
+                    
+                }
+                res.json(profile);
+            })
+            .catch(error => console.log(error));
+})
+
 module.exports = router;
