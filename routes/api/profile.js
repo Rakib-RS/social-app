07dbc9d2a2b8ff68e084cd0622313f12a6606d4b
rakib-rs <rakib.rscse@gmail.com>
2020-03-25 13:24:30 +0600

    create update profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,28 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const mongoose = require('mongoose');
-const Profile = require('../../models/profile');
-const User = require('../../models/user');
+const passport = require("passport");
+const mongoose = require("mongoose");
+const Profile = require("../../models/profile");
+const User = require("../../models/user");
 
-router.get('/test',(req,res)=> res.json({msg:'profile works'}));
+router.get("/test", (req, res) => res.json({ msg: "profile works" }));
 
 //@route get api/profile/
 //@desc return profile
 //@@ access private
 
-router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
-    const errors ={};
-    Profile.findOne({user: req.user.id})
-            .then(profile =>{
-                if(!profile){
-                    errors.noprofile = 'no profile found';
-                    return res.status(404).json(errors);
-                    
-                }
-                res.json(profile);
-            })
-            .catch(error => console.log(error));
-})
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "no profile found";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(error => console.log(error));
+  }
+);
+
+//@route post api/profile/
+//@desc create or edit user profile
+//@@ access private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubausername)
+      profileFields.githubausername = req.body.githubausername;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (typeof req.body.skills !== "undefined")
+      profileFields.skills = req.body.skills.split(",");
+    //social fields
+    profileFields.socail = {};
+    if (req.body.youtube) profileFields.youtube = req.body.youtube;
+    if (req.body.facebook) profileFields.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.linkedin = req.body.linkedin;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        //update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        //check handle
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          const errors = {};
+          if (profile) {
+            errors.handle = "handle already exist";
+            res.status(400).json(errors);
+          }
+        });
+
+        //create
+
+        new Profile(profileFields).save().then(profile => {
+          res.json(profile);
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
