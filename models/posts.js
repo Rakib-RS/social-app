a1b4abf13f748c550899a80f47c33d51e2124e80
rakib-rs <rakib.rscse@gmail.com>
2020-03-26 20:48:27 +0600

    creating the postmodel

---
+++ models/posts.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostShcema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: Date,
+      default: Date.now
+    }
+  ],
+  date: Date,
+  default: Date.now
+});
+module.exports = Posts = mongoose.model('posts',PostShcema);
