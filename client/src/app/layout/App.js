0b5d0e620d1b649b7a5709761b776b61beb674ec
rakib-rs <rakib.rscse@gmail.com>
2020-04-02 20:16:53 +0600

    navbar ,landing,footer page designed

--- client/src/app/layout/App.js
+++ client/src/app/layout/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component, Fragment } from "react";
+import NavBar from "./NavBar";
+import Footer from "./footer";
+import Landing from "./Landing";
+//import { Router, Route } from 'react-router-dom';
+import "./App.css";
+import { Container} from "semantic-ui-react";
+import {Route} from  'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-     <h1>ok</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <Route exact path='/' component={Landing}/>
+          <Footer />
+        </Container>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
-const rootEl = document.getElementById('root');
-let render = () =>{
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+const rootEl = document.getElementById("root");
+let render = () => {
   ReactDOM.render(
-    <App/>,
+    <BrowserRouter>
+      <App />  
+    </BrowserRouter>,
     rootEl
-  )
+  );
 };
-if(module.hot){
-  module.hot.accept('./app/layout/App',() =>{
-    setTimeout(render)
-  })
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 render();
 serviceWorker.unregister();
