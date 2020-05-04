b989dcd5bb6418face850a79567bfe59375cae37
rakib-rs <rakib.rscse@gmail.com>
2020-05-04 17:05:18 +0600

    composeWithDevTools

--- client/src/app/store/configureStore.js
+++ client/src/app/store/configureStore.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer/rootReducer";
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 const middleware = [thunk];
 const initialState = {};
 
@@ -8,10 +9,9 @@ const configureStore = () => {
   const store = createStore(
      rootReducer,
     initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+      applyMiddleware(...middleware)
+      
     )
   );  
 
